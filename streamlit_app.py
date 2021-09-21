03adbbcf87a021f743d15778fefa9dc13c74a4c7
samhaar <68865950+samhaar@users.noreply.github.com>
2021-09-21 15:44:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,12 @@ import streamlit as st
 """
 # Hello this is a little Streamlit App!
 # Hi there
+# Hi there
+# Hi there
+# Hi there
+# Hi there
+# Hi there
+# Hi there
 """
 
 st.slider("I'm a slider!", 0, 10, 5)
